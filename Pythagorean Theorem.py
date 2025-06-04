19b34947c0a8a14f5a883fc83f8c167ef22085f3
VERSIONpy <srivardhansirikonda02@gmail.com>
2025-06-04 23:20:03 +0530

    Create Pythagorean Theorem.py

---
+++ Pythagorean Theorem.py
@@ -0,0 +1,8 @@
+# Write code below 💖
+a = int(input("Enter lenght of short side: "))
+b = int(input("Enter lenght of another short side: "))
+c = (a**2 + b**2)
+c_new = c**0.5
+print("length of the hypotenuse is : ",c_new)
+# This code calculates the length of the hypotenuse of a right triangle using the Pythagorean theorem.
+# The Pythagorean theorem states that in a right triangle, the square of the length of the hypotenuse (c) is equal to the sum of the squares of the lengths of the other two sides (a and b).
